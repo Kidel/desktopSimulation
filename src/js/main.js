79efb197fde3eb9b939074031476c02cc9dac26f
Kidel <tano.design@gmail.com>
2015-10-16 22:21:55 +0200

    window and app icon position init to avoid overlapping at start

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,33 @@ $(function() {
     $(".window").draggable({ containment: "#drag-area", handle: ".window-handle" });
 });
 
+//window position init
+$(".window").each(function() {
+    var top = parseInt(parseInt($("#desktop").css("height"), 10)/2-300);
+    var left = parseInt(parseInt($("#desktop").css("width"), 10)/2-400);
+    $(".window").each(function() {
+        $(this).css("top", top);
+        $(this).css("left", left);
+        top += 20;
+        left += 20;
+    });
+});
+
+//app position init
+$(".app").each(function() {
+    var maxH = parseInt($("#desktop").css("height"), 10);
+    var top = 20
+    var left = 20;
+    $(".app").each(function() {
+        $(this).css("top", top);
+        $(this).css("left", left);
+        top += 85;
+
+        if(top+85>maxH)
+            left += 95;
+    });
+});
+
 $(document).ready(function(){
     document.oncontextmenu = function() {return false;};
     $(".window").mousedown(function(e){
@@ -35,12 +62,27 @@ $(document).ready(function(){
     });
 });
 
-function openApp(appName) {
-    $(".window-" + appName).show();
-    //TODO: apply max+1 current z-index, got from all open windows.
+function get_max_z(){
+    var max = 0;
+    $(".window").each(function() {
+        var index_current = parseInt($(this).css("zIndex"), 10);
+        if (index_current > max) {
+            max = index_current;
+        }
+    });
+    return max;
+    //console.log(max);
 }
+
+function openApp(appName) {
+    $(".window-" + appName).css("z-index", (get_max_z()+1));
+    $(".window-" + appName).show();
+}
+
 function closeApp(appName) {
     $(".window-" + appName).hide();
 }
 
 
+// TODO
+// color app icon when single clicked, remove color when double clicked or when another app is selected
