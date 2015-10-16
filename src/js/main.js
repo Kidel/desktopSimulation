23004e254735b5aad4c06479a90394fd01c89b4e
Kidel <tano.design@gmail.com>
2015-10-16 21:37:04 +0200

    made test app and test window, plus right click event handler and window drag with handle.

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,46 @@
 $(function() {
     $(".app").draggable({ containment: "#drag-area" });
+    $(".window").draggable({ containment: "#drag-area", handle: ".window-handle" });
 });
-$(function() {
-    $(".window").draggable({ containment: "#drag-area" });
+
+$(document).ready(function(){
+    document.oncontextmenu = function() {return false;};
+    $(".window").mousedown(function(e){
+        if( e.button == 2 ) {
+            alert('Right mouse button on window');
+            return false;
+        }
+        return true;
+    });
+    $("#home-menu").mousedown(function(e){
+        if( e.button == 2 ) {
+            alert('Right mouse button on home menu');
+            return false;
+        }
+        return true;
+    });
+    $(".app").mousedown(function(e){
+        if( e.button == 2 ) {
+            alert('Right mouse button on app');
+            return false;
+        }
+        return true;
+    });
+    $("#desktop").mousedown(function(e){
+        if( e.button == 2 ) {
+            alert('Right mouse button on desktop');
+            return false;
+        }
+        return true;
+    });
 });
 
 function openApp(appName) {
-    $(".window-"+appName).show();
-    //TODO: apply max+1 current z-index, stored in a global var and update the var.
+    $(".window-" + appName).show();
+    //TODO: apply max+1 current z-index, got from all open windows.
+}
+function closeApp(appName) {
+    $(".window-" + appName).hide();
 }
 
 
